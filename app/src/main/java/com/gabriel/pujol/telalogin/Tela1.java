736af20ff07bfca6d8e9768c9ec67458fde85632
Gabriel Santos Pujol <gabriel-pujol@hotmail.com>
2016-12-16 23:40:58 -0200

    Adicionando projeto

---
+++ app/src/main/java/com/gabriel/pujol/telalogin/Tela1.java
@@ -0,0 +1,13 @@
+package com.gabriel.pujol.telalogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tela1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela1);
+    }
+}
